45638a40d4921525e51cc0fcb2acc3a0de699fe5
PRATIKHM <pratik.236147101@vcet.edu.in>
2024-10-05 12:49:43 +0530

    Add files via upload

---
+++ Assignment No.8.java
@@ -0,0 +1,18 @@
+//Q8.Remove all the white spaces from the string passed to the method and return the modified string.
+class Tester{
+
+    public static String removeWhiteSpaces(String str){
+		 return str.replaceAll("\\s+", "");//Implement your code here and change the return value accordingly
+        
+	}
+	
+	public static void main(String args[]){
+		String str = "Hello good afternoon   ";
+		str = removeWhiteSpaces(str);
+		System.out.println(str);
+	}
+}
+
+
+//Output:
+//Hellogoodafternoon
